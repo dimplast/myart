07f5f91716b3860292dcbf535bde5dedbcee4832
dimplast <dimplast@gmail.com>
2021-09-23 16:33:50 +0300

    second commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<div class="fixed w-screen z-40 bg-black p-4 shadow-2xl">
+  <div class="text-center text-white font-carattere text-6xl">Vik P</div>
+</div>
+
+<div class="z-0 flex flex-col lg:flex-row bg-black">
+  <div class="flex items-end justify-center h-screen w-screen bg-contain bg-center bg-origin-content bg-no-repeat lg:w-1/3 scroll-snap-start" style="background-image: url('/img/art5.jpg');">
+    <div class="text-white font-petit text-5xl">The Devil You Know</div>
+  </div>
+  <div class="flex items-end justify-center h-screen w-screen bg-contain bg-center bg-origin-content bg-no-repeat lg:w-1/3 scroll-snap-start" style="background-image: url('/img/art6.jpg');">
+    <div class="text-white font-petit text-5xl">Three of Swords</div>
+  </div>
+  <div class="flex items-end justify-center h-screen w-screen bg-contain bg-center bg-origin-content bg-no-repeat lg:w-1/3 scroll-snap-start" style="background-image: url('/img/art1.jpg');">
+    <div class="text-white font-petit text-5xl">Oblivion</div>
+  </div>
+  <div class="flex items-end justify-center h-screen w-screen bg-contain bg-center bg-origin-content bg-no-repeat lg:w-1/3 scroll-snap-start" style="background-image: url('/img/art2.jpg');">
+    <div class="text-white font-petit text-5xl">The Judge</div>
+  </div>
+  <div class="flex items-end justify-center h-screen w-screen bg-contain bg-center bg-origin-content bg-no-repeat lg:w-1/3 scroll-snap-start" style="background-image: url('/img/art3.jpg');">
+    <div class="text-white font-petit text-5xl">No rest for the wicked</div>
+  </div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+const config = {
+	mode: "jit",
+	purge: [
+		"./src/**/*.{html,js,svelte,ts}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#FF6363',
+				secondary: {
+					100: '#E2E2D5',
+					200: '#888883'
+				  }
+			},
+			fontFamily: {
+				petit: ['Italianno'],
+				carattere: ['Carattere']
+			}
+		}
+	},
+	plugins: []
+};
+
+module.exports = config;
